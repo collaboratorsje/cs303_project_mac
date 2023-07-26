d582990f71b6eb0e66cd0358f430a89ecad47f09
Seth Emery <37260478+collaboratorsje@users.noreply.github.com>
2023-07-25 19:28:04 -0500

    Update website.cpp

--- Source-code_mac/website.cpp
+++ Source-code_mac/website.cpp
@@ -8,7 +8,7 @@ Website::Website() {
     minute = 0;
     second = 0;
 }
-
+// constructor
 Website::Website(const std::string &websiteName, int year, int month, int day,
                  int minute, int second) {
     this->name = websiteName;
@@ -18,7 +18,7 @@ Website::Website(const std::string &websiteName, int year, int month, int day,
     this->minute = minute;
     this->second = second;
 }
-
+// setters and getters
 std::string Website::getName() const { return name; }
 
 void Website::setName(const std::string &websiteName) {
@@ -44,13 +44,13 @@ void Website::setMinute(int minute) { this->minute = minute; }
 int Website::getSecond() const { return second; }
 
 void Website::setSecond(int second) { this->second = second; }
-
+// print function
 void Website::print() const {
     std::cout << "Website: " << name << std::endl;
     std::cout << "Date Visited: " << year << "-" << month << "-" << day
               << std::endl;
 }
-
+// sort function by date compares the website objects to eachother and returns a bool after using a cascading approach of comparing years, months, etc.
 bool Website::sortByDate(const Website &w1, const Website &w2) {
     if (w1.getYear() != w2.getYear())
         return w1.getYear() < w2.getYear();
@@ -62,11 +62,11 @@ bool Website::sortByDate(const Website &w1, const Website &w2) {
         return w1.getMinute() < w2.getMinute();
     return w1.getSecond() < w2.getSecond();
 }
-
+// sort function by name compares alphabetically the two website objects names and returns a bool if the first's name is < the second's name
 bool Website::sortByName(const Website& w1, const Website& w2) {
     return w1.getName() < w2.getName();
 }
-
+// This function is made so I could convert a Qstring object from the Qlistwidget on QT into a string so I could compare it to other website objects in the list
 bool Website::compareData(const QString& otherData) const {
     // Assuming "name" is the relevant data to compare with the QString
     return QString::fromStdString(name).compare(otherData, Qt::CaseInsensitive) == 0;
